4f81ccc2cea9dbd6b1a4e9e29d566c3a7a7e11d6
collin-beck3 <collin.beck3@gmail.com>
2026-04-13 18:25:28 -0400

    Phase 1 complete, creatd minimal loop and quit function when prompted.

--- java/src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ java/src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -1,8 +1,32 @@
 package com.github.zipcodewilmington;
 
+import java.util.Scanner;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
-public class WordGuess {}
+// Collin Beck 
+
+// Phase 1: Create a simple word guess game that prompts the user to guess a letter 
+// and displays the guessed letter back to the user. The game should continue until 
+// the user types "quit" to exit the game. 
+
+public class WordGuess {
+ public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     System.out.println("Welcome to the Word Guess Game!");
+
+     while (true) { 
+         System.out.print("Guess a letter (or type 'quit' to exit): ");
+         String guess = scanner.nextLine();
+         if (guess.equalsIgnoreCase("quit")) {
+             System.out.println("Thanks for playing!");
+             scanner.close();
+             break;
+         }
+         System.out.println("You guessed: " + guess);
+     }
+    }
+}
